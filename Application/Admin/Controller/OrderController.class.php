f4657a4b38d06a611d606a90beda9687da9c8ed4
shenyan <85464277@qq.com>
2018-05-11 09:47:42 +0800

    csv格式的Excel

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -1,25 +1,35 @@
 <?php
 namespace Admin\Controller;
-
 use Think\Controller;
-
 class OrderController extends Controller {
 
+    /**
+     * PHPExcel导出功能
+     */
     public function excelDemo(){
-    	$header = array(
-
+        $header = array(
             array(NULL, 沈唁, 集成, Excel)
-
-        	);
+        );
         $orderinfo = array(
-
-        	array(1,2,3,4),
-        	array(5,6,7,8),
-        	array(1,3,5,7)
-
-        	);
+            array(1,2,3,4),
+            array(5,6,7,8),
+            array(1,3,5,7)
+        );
         $data = array_merge($header,$orderinfo);
         createXls($data);
     }
 
+    /**
+     * 导出csv格式的Excel
+     */
+    public function csvDemo()
+    {
+        $data = array(
+            '1,2,3,4,5',
+            '6,7,8,9,0',
+            '1,3,5,7,9'
+        );
+        $header='ID,用户名,密码,性别,手机号';
+        createCsv($data,$header);
+    }
 }
