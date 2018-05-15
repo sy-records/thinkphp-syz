4734df9f42502fef669ed50cb69091c20da28b9b
shenyan <85464277@qq.com>
2018-05-15 11:30:53 +0800

    微信信息配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,12 +10,11 @@ return array(
 		'__PUBLIC_HOME__'=>'/Public/Home',
 		),
 	'SHOW_PAGE_TRACE'=>true,
-	/* 数据库设置 */
-    'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  '',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'root',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'jx_',    // 数据库表前缀
+    'WECHAT' => [
+        'appid' => '',
+        'appKey' => '',
+        'apiKey' => '',
+        'mchid' => '',
+        'certificate_path' => dirname(dirname(__FILE__)).'/Credential',
+    ],
 );
