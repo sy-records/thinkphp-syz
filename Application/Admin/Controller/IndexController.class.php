a0f65de3819d88ced6acf201fcf709f03574319c
shenyan <52o@qq52o.cn>
2018-05-16 09:32:59 +0800

    邮件调用

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,8 +3,19 @@ namespace Admin\Controller;
 use Think\Controller;
 
 class IndexController extends Controller {
-    public function index(){
-
-    	echo 111;
+    /**
+     * 发送邮件demo
+     * @throws \phpmailerException
+     */
+    public function sendMail()
+    {
+        $to = '52o@qq52o.cn';
+        $subject = '邮件标题';
+        $content = '邮件内容';
+        if(sendMail($to,$subject,$content)){
+            $this->success('发送成功');
+        }else{
+            $this->error('发送失败');
+        }
     }
 }
