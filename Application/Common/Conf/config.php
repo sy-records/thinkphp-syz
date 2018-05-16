8ef6a98e4751372e8dc4716b2262bb45de50560d
shenyan <52o@qq52o.cn>
2018-05-16 09:32:05 +0800

    添加邮件读取配置信息

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -17,4 +17,13 @@ return array(
         'mchid' => '',
         'certificate_path' => dirname(dirname(__FILE__)).'/Credential',
     ],
+    // 配置邮件发送服务器
+    'MAIL_SMTP'                     =>TRUE,
+    'MAIL_HOST'                     =>'smtp.exmail.qq.com',//邮件发送SMTP服务器
+    'MAIL_SMTPAUTH'                 =>TRUE,
+    'MAIL_USERNAME'                 =>'52o@qq52o.cn',//SMTP服务器登陆用户名
+    'MAIL_PASSWORD'                 =>'XXXXXXXXXXXXXXXXXXXX',//SMTP服务器登陆密码
+    'MAIL_SECURE'                   =>'tls',
+    'MAIL_CHARSET'                  =>'utf-8',
+    'MAIL_ISHTML'                   =>TRUE,
 );
