a2cbadd9377e4aede11503c5e415196224944c0f
shenyan <52o@qq52o.cn>
2018-05-16 09:29:01 +0800

    封装发送邮件方法

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -85,3 +85,39 @@ function createCsv($data,$header=null,$filename='orderlist.csv'){
         echo iconv('UTF-8','GBK',$v)."\t\r\n";
     }
 }
+
+/**
+ * 发送邮件
+ * @param $to
+ * @param $subject
+ * @param $content
+ * @return bool
+ * @throws phpmailerException
+ */
+function sendMail($to, $subject, $content){
+    Vendor('phpmailer.class#phpmailer');
+    $mail = new \PHPMailer(); //实例化
+    // 装配邮件服务器
+    if (C('MAIL_SMTP')) {
+        $mail->IsSMTP();  //启动SMTP
+    }
+    $mail->Host = C('MAIL_HOST'); //SMTP服务器地址
+    $mail->SMTPAuth = C('MAIL_SMTPAUTH'); //启用SMTP认证
+    $mail->Username = C('MAIL_USERNAME');//邮箱名称
+    $mail->Password = C('MAIL_PASSWORD');//邮箱密码
+    $mail->SMTPSecure = C('MAIL_SECURE');//发件人地址
+    $mail->CharSet = C('MAIL_CHARSET');//邮件头部信息
+    $mail->From = C('MAIL_USERNAME');//SMTP服务器登陆用户名
+    $mail->AddAddress($to);
+    $mail->FromName = '十年之约项目组'; //发件人是谁
+//    $mail->AddAttachment('./Public/test.png','沈唁志.png'); // 添加附件,并指定名称
+    $mail->IsHTML(C('MAIL_ISHTML'));//是否是HTML字样
+    $mail->Subject = $subject;// 邮件标题信息
+    $mail->Body = $content;//邮件内容
+    // 发送邮件
+    if (!$mail->Send()) {
+        return FALSE;
+    } else {
+        return TRUE;
+    }
+}
