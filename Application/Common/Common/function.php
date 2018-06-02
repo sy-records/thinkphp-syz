840620137590876a19d0288377299805372d9946
shenyan <52o@qq52o.cn>
2018-06-02 09:25:20 +0800

    封装二维码类

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -142,3 +142,13 @@ function getGravatar($email, $s = 96, $d = 'mp', $r = 'g', $img = false, $atts =
     }
     return  $url;
 }
+
+/**
+ * 生成二维码
+ * @param  string  $url  url连接
+ * @param  integer $size 尺寸 纯数字
+ */
+function qrcode($url,$size=4){
+    Vendor('Phpqrcode.phpqrcode');
+    QRcode::png($url,false,QR_ECLEVEL_L,$size,2,false,0xFFFFFF,0x000000);
+}
