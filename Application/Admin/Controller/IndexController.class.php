b71b1e9feefb789f4406007dcf2a971275981577
fphd_xian_lufei <fphd_xian_lufei@fphd>
2018-09-26 10:33:30 +0800

    发送消息到钉钉机器人 	modified:   Application/Admin/Controller/IndexController.class.php

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -30,4 +30,34 @@ class IndexController extends Controller {
         $imgUrl = getGravatar($email);
         echo "<img src=" . $imgUrl . " />";
     }
+
+    /**
+     * 发送消息到钉钉机器人
+     * 文档地址: https://open-doc.dingtalk.com/docs/doc.htm?treeId=257&articleId=105735&docType=1
+     * @param $title 标题
+     * @param string $description 消息描述
+     * @param string $link 内容链接，$messageType为link时，此参数不能为空
+     * @param string $messageType 消息类型，link，markdown
+     * @param string $robotUrl 所使用的机器人地址
+     * @param string $data 完整内容
+     * @return mixed
+     */
+    public function sendMessageToDingdingRobot($title, $description = '点击查看', $link = '', $messageType = 'link', $robotUrl = '', $data = '内容为空')
+    {
+        if ($messageType == 'link') {
+            $json = json_encode([
+                'msgtype' => 'link',
+                'link' => [
+                    'title' => $title,
+                    "messageUrl" => $link,
+                    "text" => $description
+                ]
+            ]);
+        } elseif ($messageType == 'markdown') {
+            // markdown 消息
+        }
+
+        $result = myCurl($robotUrl, $json , 1);
+        return $result;
+    }
 }
