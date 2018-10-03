2f69802888d1c0f91824d60d04f80ad2388fa089
shenyan <52o@qq52o.cn>
2018-10-03 19:45:57 +0800

    使用webhook自动部署项目代码

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -60,4 +60,42 @@ class IndexController extends Controller {
         $result = myCurl($robotUrl, $json , 1);
         return $result;
     }
+
+    /**
+     * 使用webhook自动部署项目代码
+     * @return int
+     */
+    public function gitWebHook()
+    {
+        $target = '/www/wwwroot/WordPress-tools'; // 生产环境web目录
+        //密钥
+        $secret = "test6666";
+        //获取GitHub发送的内容
+        $json = file_get_contents('php://input');
+        $content = json_decode($json, true);
+        //github发送过来的签名
+        $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        if (!$signature) {
+            return http_response_code(404);
+        }
+        list($algo, $hash) = explode('=', $signature, 2);
+        //计算签名
+        $payloadHash = hash_hmac($algo, $json, $secret);
+        // 判断签名是否匹配
+        if ($hash === $payloadHash) {
+            $cmd = "cd $target && git pull";
+            $res = shell_exec($cmd);
+            $res_log = 'Success:'.PHP_EOL;
+            $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
+            $res_log .= $res.PHP_EOL;
+            $res_log .= '======================================================================='.PHP_EOL;
+            echo $res_log;
+        } else {
+            $res_log  = 'Error:'.PHP_EOL;
+            $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
+            $res_log .= '密钥不正确不能pull'.PHP_EOL;
+            $res_log .= '======================================================================='.PHP_EOL;
+            echo $res_log;
+        }
+    }
 }
