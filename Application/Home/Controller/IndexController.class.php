50b9d166bb7f34d739b5383ae554ca84316c4f7d
沈唁 <52o@qq52o.cn>
2019-01-15 21:16:57 +0800

    Update IndexController.class.php

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,32 +5,32 @@ use Think\Controller;
 
 class IndexController extends Controller {
 
-	/**
-	 * $url 链接
-	 * $size 大小
-	 */
-	public function qrcode()
+    /**
+     * $url 链接
+     * $size 大小
+     */
+    public function qrcode()
     {
-		$url = "https://qq52o.me/";
-		$size = '6';
-		qrcode($url, $size);
-	}
+        $url = "https://qq52o.me/";
+        $size = '6';
+        qrcode($url, $size);
+    }
 
-	/**
-	 * 封装一个P方法打印数组
-	 * @return [type] [description]
-	 */
-	public function p()
+    /**
+     * 封装一个P方法打印数组
+     * @return [type] [description]
+     */
+    public function p()
     {
-		$array = array(
-			't0' => 'test0',
-			't1' => 'test1',
-			't2' => array(
-				'tt0' => 'test0',
-				'tt1' => 'test1',
-			),
-		);
-		p($array);
-	}
-	
+        $array = array(
+            't0' => 'test0',
+            't1' => 'test1',
+            't2' => array(
+                'tt0' => 'test0',
+                'tt1' => 'test1',
+            ),
+        );
+        p($array);
+    }
+
 }
