6e59ef65433b00b819a4b3d852dc7c64ba97edba
沈唁 <52o@qq52o.cn>
2019-02-17 17:13:51 +0800

    emoji表情转义

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -332,3 +332,37 @@ function setOrderId($num, $prefix = "")
 
 	return $prefix.$code;
 }
+
+/**
+* 对emoji表情转义
+* @param $str
+* @return string
+*/
+function emoji_encode($str){
+	$strEncode = '';
+
+	$length = mb_strlen($str,'utf-8');
+
+	for ($i=0; $i < $length; $i++) {
+	    $_tmpStr = mb_substr($str,$i,1,'utf-8');
+	    if(strlen($_tmpStr) >= 4){
+		$strEncode .= '[[EMOJI:'.rawurlencode($_tmpStr).']]';
+	    }else{
+		$strEncode .= $_tmpStr;
+	    }
+	}
+	return $strEncode;
+}
+
+/**
+* 对emoji表情转反义
+* @param $str
+* @return null|string|string[]
+*/
+function emoji_decode($str){
+	$strDecode = preg_replace_callback('|\[\[EMOJI:(.*?)\]\]|', function($matches){
+		return rawurldecode($matches[1]);
+	}, $str);
+
+	return $strDecode;
+}
